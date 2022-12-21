7f264efb0ff80553a6b7281738946f7420443bc5
MaximGol <maxim.gol500@mail.ru>
2022-12-21 18:26:50 +0300

    polirovka

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Blog_Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(max_length=250)
+    description = models.TextField(max_length=2500)
     date = models.DateField()
 
 # Create your models here.
+    def __str__(self):
+            return self.title
